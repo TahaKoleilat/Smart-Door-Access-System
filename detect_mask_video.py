1e20bc6b69b091b118248b8315a69991a92bdcf4
iaf07@mail.aub.edu <islam.fares@eigentec.de>
2022-10-01 09:03:29 +0300

    fixed real time predicitions

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -18,7 +18,7 @@ class MaskDetectionModel(object):
 
     def predict_mask(self, img):
         self.preds = self.loaded_model.predict(img)
-        return MaskDetectionModel.MASK_STATE[np.argmax(self.preds)]
+        return self.preds
 
 
 facec = cv2.CascadeClassifier(
@@ -37,23 +37,28 @@ class VideoCamera(object):
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
         ret, fr = self.video.read()
-        # gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+        fr = cv2.cvtColor(fr, cv2.COLOR_BGR2RGB)
         faces = facec.detectMultiScale(fr, 1.3, 5)
 
         for (x, y, w, h) in faces:
             fc = fr[y:y+h, x:x+w]
 
             roi = cv2.resize(fc, (128, 128), interpolation=cv2.INTER_AREA)
-
-            pred = model.predict_mask(
-                roi[np.newaxis, :, :, np.newaxis].resize(128, 128, 3))
-
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            pred = model.predict_mask(roi[np.newaxis, :, :, ])
+            if (pred >= 0.5):
+                cv2.putText(
+                    fr, MaskDetectionModel.MASK_STATE[1], (x, y), font, 1, (255, 255, 0), 2)
+            else:
+                cv2.putText(
+                    fr, MaskDetectionModel.MASK_STATE[0], (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            # Capture frame-by-frame
 
         if ret == True:
             cv2.imshow('Frame', fr)
+
+        if cv2.waitKey(1) == ord('q'):
+            self.video.release()
+            cv2.destroyAllWindows()
 
         _, jpeg = cv2.imencode('.jpg', fr)
         return jpeg.tobytes()
